18211a4075eaaf13da24dc4f812c517e4da558af
Mincong <mincong.zhang@hotmail.com>
2016-05-27 15:23:49 +0100

    Create awk.sh

---
+++ Bash/awk.sh
@@ -0,0 +1,5 @@
+#awk '{print $3, $5}':
+#Awk breaks each line of input passed to it into fields.Then print out $3 and $5 fields
+
+#awk: -F option to specify ":" as the field separator
+#e.g.:  awk -F ';' '{print $21}'
