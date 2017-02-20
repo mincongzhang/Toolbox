3e9d44e9fa754bdbf53681fa4b33cf914fa38c4f
Mincong <mincong.zhang@hotmail.com>
2017-02-20 16:31:45 +0000

    Update awk.sh

--- Bash/awk.sh
+++ Bash/awk.sh
@@ -5,3 +5,8 @@
 #e.g.:  awk -F ';' '{print $21}'
 
 cat file | grep sth | awk '{print $3, $5}'
+
+#dump when 11th column is not empty
+column="11"
+filter=""
+cat $dump_file | awk -F '|' -v col=$column -v fil=$filter '$col!=fil {print}' 
