b4f755562fbffceae390eaaae3f2faa6a17735f0
Mincong <mincong.zhang@hotmail.com>
2017-05-09 10:26:33 +0100

    Create cauture_backticks.pl

---
+++ Perl/cauture_backticks.pl
@@ -0,0 +1,5 @@
+# https://www.effectiveperlprogramming.com/2010/04/when-perl-isnt-enough/
+
+$trans = "Hello, today is `date +%Y%m%d`, yesterday is `date --date=\"-1 day\" +%Y%m%d`!";
+$result = qx{echo $trans};
+print $result;
