8bdef454f2ece6f1a1d3848a477e89d50eb7f5cb
Mincong <mincong.zhang@hotmail.com>
2017-06-29 13:52:52 +0100

    Update map_find.cpp

--- CppTools/STL/map_find.cpp
+++ CppTools/STL/map_find.cpp
@@ -1,1 +1,10 @@
-sth
+  template<typename Key,typename Value>
+  bool get_map_value(const std::map<Key,Value> & map, const Key & key,Value & value) {
+    typename std::map<Key,Value>::const_iterator map_it = map.find(key);
+    if(map_it!=map.end()){
+      value = map_it->second;
+      return true;
+    }
+
+    return false;
+  }
