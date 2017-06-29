26807198520be145127a32a83dc7c1df976e53a1
Mincong <mincong.zhang@hotmail.com>
2017-06-29 14:54:32 +0100

    Update easy_log.cpp

--- CppTools/easy_log.cpp
+++ CppTools/easy_log.cpp
@@ -1,1 +1,3 @@
-#define logInfo(msg) do{ std::cout<<msg<<std::endl;}while(0)
+#define logInfo(msg) do{std::cout<<msg<<std::endl;}while(0)
+
+#define logError(msg) do{logInfo("ERROR:"<<msg);}while(0)
