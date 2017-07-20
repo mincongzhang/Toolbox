e00df3e0da1467e54c93fcc2d06823927149e960
Mincong <mincong.zhang@hotmail.com>
2017-07-20 11:28:54 +0100

    Update split.cxx

--- CppTools/Split/split.cxx
+++ CppTools/Split/split.cxx
@@ -1,7 +1,33 @@
 
+///////////////////////////////////////////////////////////////////
+// spaces ' ' only
+///////////////////////////////////////////////////////////////////
+#include <vector>
+#include <string>
+#include <sstream>
 
+void split(const std::string & s, std::vector<std::string> & out){
+  std::stringstream ss(s); // Insert the string into a stream
+  std::string buf; // Have a buffer string
 
-//boost
+  while (ss >> buf){
+    out.push_back(buf);
+  }
+}
+
+int main() {
+  std::string line("12 14 L");
+  std::vector<std::string> out;
+  split(line,out);
+
+  std::cout<<out<<std::endl;
+
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////////
+// boost
+///////////////////////////////////////////////////////////////////
 #include <boost/algorithm/string.hpp>
 std::vector<std::string> strs;
 boost::split(strs, "string to split", boost::is_any_of("\t "));
