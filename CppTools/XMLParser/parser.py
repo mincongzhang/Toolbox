0bc2ee0080d8861bbdd42bd9f3a0181a00c506bf
Mincong <mincong.zhang@hotmail.com>
2017-10-10 16:59:26 +0100

    Create parser.py

---
+++ CppTools/XMLParser/parser.py
@@ -0,0 +1,13 @@
+from xml.dom import minidom
+from xml.dom.minidom import parse
+
+xmldoc = minidom.parse('sample.xml')
+
+# find the name element, if found return a list, get the first element
+name_element = xmldoc.getElementsByTagName("name")[0]
+
+# this will be a text node that contains the actual text
+text_node = name_element.childNodes[0]
+
+# get text
+print text_node.data
