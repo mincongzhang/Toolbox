25a8782f3a7169888745d4c1856581e9011704b7
Mincong <mincong.zhang@hotmail.com>
2017-10-31 17:04:00 +0000

    Create replace.py

---
+++ PythonTools/replace.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import fileinput
+
+replace_file = open("replace.csv","r")
+replace_map = {}
+for line in replace_file:
+    str_line = line.strip();
+    line_elems = str_line.split(",")
+    print(line_elems[0]+":"+line_elems[1].lower())
+    replace_map[line_elems[0]] = line_elems[1].lower()
+
+print("replace_map:")
+print(replace_map)
+replace_file.close();
+
+print("replacing")
+for key, value in replace_map.items():
+    print("replacing:"+key+"->"+value)
+
+    with fileinput.FileInput("quote_fields.xml", inplace=True, backup='.bak') as file:
+        for line in file:
+            print(line.replace(key, value), end='')
