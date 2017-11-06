0f89a5d256f0e51007cf2aa32b9f447a5756aa74
Mincong <mincong.zhang@hotmail.com>
2017-11-06 17:29:04 +0000

    Create xml_parser.py

---
+++ PythonTools/xml_parser.py
@@ -0,0 +1,32 @@
+from xml.dom import minidom
+from xml.dom.minidom import parse
+
+xmldoc = minidom.parse('sample.xml')
+
+# find the name element, if found return a list, get the first element
+name_element = xmldoc.getElementsByTagName("name")[0]
+
+# this will be a text node that contains the actual text
+text_node = name_element.childNodes[0]
+
+# get text
+print text_node.data
+
+# Go through the whole xml
+print "for loop:"
+for name_element in xmldoc.getElementsByTagName("name"):
+    text_node = name_element.childNodes[0]
+    print text_node.data
+
+########################################
+# Sub elements
+########################################
+
+xmldoc = minidom.parse('quote_fields.xml')
+
+# Go through the whole xml
+print("for loop:")
+for field_element in xmldoc.getElementsByTagName("field"):
+    for format in field_element.getElementsByTagName("format"):
+        text_node = format.childNodes[0]
+        print(text_node.data)
