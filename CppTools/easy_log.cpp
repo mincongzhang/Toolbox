8ff3aa4792e0103f2f1451d12ec3129f2852bd2d
Mincong <mincong.zhang@hotmail.com>
2018-04-11 11:22:24 +0100

    Update easy_log.cpp

--- CppTools/easy_log.cpp
+++ CppTools/easy_log.cpp
@@ -1,3 +1,6 @@
+#define BOLDWHITE(msg) "\033[1;37m"<<msg<<"\033[0m"
+#define BOLDRED(msg) "\033[1;31m"<<msg<<"\033[0m"
+#define BOLDGREEN(msg) "\033[1;32m"<<msg<<"\033[0m"
 #define logInfo(msg) do{std::cout<<msg<<std::endl;}while(0)
-
-#define logError(msg) do{logInfo("ERROR:"<<msg);}while(0)
+#define logHighlight(msg) do{logInfo(BOLDGREEN(msg));}while(0)
+#define logError(msg) do{std::cerr<<BOLDRED("ERROR:")<<msg<<std::endl;}while(0)
