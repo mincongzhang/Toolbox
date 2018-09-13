ae4d2893f7e88b12d922f47e97e41e44cfe17cc6
Mincong <mincong.zhang@hotmail.com>
2018-09-13 11:18:23 +0100

    Update trim_empty_newlines.py

--- PythonTools/trim_empty_newlines.py
+++ PythonTools/trim_empty_newlines.py
@@ -10,7 +10,8 @@ reduce double new lines to one
 
 End"""
 
-double_newlines = "\n\n\n"
-while double_newlines in x:
-    x = x.replace("\n\n\n", "\n\n")
+double_empty_newlines = "\n\n\n"
+single_empty_newline = "\n\n"
+while double_empty_newlines in x:
+    x = x.replace(double_empty_newlines, single_empty_newline)
 print(x)
